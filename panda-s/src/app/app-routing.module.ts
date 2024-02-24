a3c8fec354f5b86a9c2c8c0be07bca3386ca5a59
Sergio Satyabrata <sergio.satyabrata@gmail.com>
2024-02-24 11:46:32 -0800

    added routings

--- panda-s/src/app/app-routing.module.ts
+++ panda-s/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { ShoepageComponent } from './shoepage/shoepage.component';
+import { StorefrontpageComponent } from './storefrontpage/storefrontpage.component';
+
+
+const routes: Routes = [
+  {path: '', component: WelcomepageComponent},
+  {path: 'storefront', component: StorefrontpageComponent},
+  {path: 'storefront/shoe/:id', component: ShoepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- panda-s/src/app/pandaproxy.service.ts
+++ panda-s/src/app/pandaproxy.service.ts
@@ -9,8 +9,17 @@ export class PandaproxyService {
 
     hostUrl:string = 'http://localhost:8080/';
 
-    constructor(private httpClient: HttpClient) { 
+    constructor(private httpClient: HttpClient) { }
+    
 
-      
-    }
+    // Needs to create some getters here...
+
+//     e.g. getListsIndex() {
+//     return this.httpClient.get<any[]>( this.hostUrl + 'json/lists.json');
+//   }
+
+//   getItems(index: string) {
+//     return this.httpClient.get( this.hostUrl + 'json/lists/' + index + '.json');
+//   }
+
 }
