4ccf3ece4f4d2549cf63154b8c8e44498b4620c1
Carmel Laurentia <116763140+Claurentia@users.noreply.github.com>
2024-02-27 15:42:27 -0800

    added test

--- panda-s/tests/test1.js
+++ panda-s/tests/test1.js
@@ -4,7 +4,11 @@ const server = 'http://localhost:8080/';
 chai.use(chaiHttp);
 const expect = chai.expect;
 
+
+
 describe('Get a single object', function() {
+
+
   it('Should return an object', function(done) {
     chai.request(server)
       .get('app/shoes/1')
@@ -14,4 +18,23 @@ describe('Get a single object', function() {
         done();
       });
   });
+
+
+  it('Should have the correct properties and values for Shoe with ID 1', function(done) {
+    chai.request(server)
+      .get('app/shoes/1')
+      .end(function(err, res) {
+  
+        expect(res.body).to.have.property('shoeName', 'Geo shoe');
+        expect(res.body).to.have.property('shoeDescription', 'A shoe that prevents you from getting lost.');
+        expect(res.body).to.have.property('shoeId', '1');
+        expect(res.body).to.have.property('shoeSize', 5.5);
+        expect(res.body).to.have.property('shoeRating', 4.5);
+        expect(res.body).to.have.property('storeId', '181818');
+        expect(res.body).to.have.property('shoePrice', 39.99);
+        expect(res.body).to.have.property('shoeQuantity', 3);
+        done();
+      });
+  });
+
 });
